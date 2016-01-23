9c4830cc1c3a91c28c8835839ec5fcbc2708d342
wenxuan-xia <wenxuan.xia@hotmail.com>
2016-01-23 18:22:47 -0500

    enw

--- xiafamily/xiafamily/familytree/models.py
+++ xiafamily/xiafamily/familytree/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class familyMember(models.Model):
+    """
+        Family member basic information.
+    """
+    name = models.CharField(null=True, max_length=16)
+    generation = models.CharField(null=True, max_length=16)
+
+    belongs_to = models.CharField(null=True, max_length=16)
+    oldsystem_id = models.IntegerField(default=0)
+
+    gender = models.CharField(default="男", max_length=4)
+    married = models.BooleanField(default=False)
+    parent_id = models.IntegerField(default=0)
+    spouse_name = models.CharField(max_length="16", default="")
+
+    education = models.TextField(null=True)
+    careers = models.TextField(null=True)
