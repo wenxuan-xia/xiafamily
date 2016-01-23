3275bad48e60190d18a22652205ed051a6bd5f17
EC2 Default User <ec2-user@ip-172-31-61-106.ec2.internal>
2016-01-23 23:11:49 +0000

    restart

--- xiafamily/xiafamily/familytree/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-class familyMemberAdmin(admin.ModelAdmin):
-    list_display = ["oldsystem_id", "name", "gender", "generation", "belongs_to"]
-
-
-admin.site.register(familyMember, familyMemberAdmin)
